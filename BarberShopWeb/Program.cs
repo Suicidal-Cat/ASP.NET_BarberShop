6b092a1e9c3f0ae015168395475576f62eef00e7
Suicidal-Cat <darekolaric@gmail.com>
2024-05-04 22:35:30 +0200

    mobile api setup

--- BarberShopWeb/Program.cs
+++ BarberShopWeb/Program.cs
@@ -7,6 +7,10 @@ using DataAccessLayer.UnitOfWork;
 using BarberShop.Services.Interfaces;
 using BarberShop.Services.ImplementationDatabase;
 using BarberShop.Domain;
+using BarberShop.Services.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +23,20 @@ builder.Services.AddScoped<IServiceService,ServiceService>();
 builder.Services.AddScoped<IServiceCategoryService, ServiceCategoryService>();
 builder.Services.AddScoped<IBarberService, BarberService>();
 builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey=true,
+			IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+			ValidateIssuer=true,
+			ValidateAudience=false,
+		};
+	});
+
+builder.Services.AddScoped<JWTService>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -36,12 +54,15 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
+app.UseAuthentication();	
 
 app.UseAuthorization();
 app.MapRazorPages();
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+	name: "MobileControllers",
+	pattern: "mobile/{controller}/{action}/{id?}");
 
 app.Run();
